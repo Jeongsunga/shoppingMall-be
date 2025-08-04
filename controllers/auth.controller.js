b2d1116844df894a947f33b52687ef99de78b503
Seunga Jeong <jsa3341@gmail.com>
2025-08-04 15:43:22 +0900

    get user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+require("dotenv").config()
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const authController = {};
 
@@ -18,6 +21,21 @@ authController.loginWithEmail = async (req, res) => {
   } catch (error) {
     return res.status(400).json({ status: "fail", error: error.message });
   }
+};
+
+authController.authenticate = async(req, res, next) => {
+    try {
+        const tokenString = req.headers.authorization;
+        if(!tokenString) throw new Error("Token not found");
+        const token = tokenString.replace("Bearer ", "");
+        jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+            if(error) throw new Error("invalid token");
+            req.userId = payload._id;
+        });
+        next();
+    } catch(error) {
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
 };
 
 module.exports = authController;
